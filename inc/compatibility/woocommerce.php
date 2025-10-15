35c8dcc5d4161fc5b193e46c051ce0ffc4c6c70f
Muzammil Hussain <smhz101@gmail.com>
2025-10-15 23:56:08 +0500

    chore(support) added woocommerce support

--- inc/compatibility/woocommerce.php
+++ inc/compatibility/woocommerce.php
@@ -10,12 +10,47 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-add_action( 'init', 'edgeframe_wc_setup' );
+add_action( 'after_setup_theme', 'edgeframe_wc_setup' );
 /**
  * WooCommerce setup function.
  *
  * @return void
  */
 function edgeframe_wc_setup() {
-	// Keep minimal, add wrappers if needed later
+	// Declare WooCommerce support and product gallery features.
+	add_theme_support( 'woocommerce', array(
+		'thumbnail_image_width' => 400,
+		'single_image_width'    => 800,
+		'product_grid'          => array(
+			'default_rows'    => 4,
+			'default_columns' => 3,
+			'min_columns'     => 2,
+			'max_columns'     => 4,
+		),
+	) );
+
+	add_theme_support( 'wc-product-gallery-zoom' );
+	add_theme_support( 'wc-product-gallery-lightbox' );
+	add_theme_support( 'wc-product-gallery-slider' );
+}
+
+/**
+ * Replace WooCommerce content wrappers with theme wrappers.
+ */
+add_action( 'init', function() {
+    // Remove default Woo wrappers.
+    remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
+    remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
+
+    // Add theme wrappers.
+    add_action( 'woocommerce_before_main_content', 'edgeframe_wc_wrapper_start', 10 );
+    add_action( 'woocommerce_after_main_content', 'edgeframe_wc_wrapper_end', 10 );
+} );
+
+function edgeframe_wc_wrapper_start() {
+    echo '<main id="primary" class="site-main">';
+}
+
+function edgeframe_wc_wrapper_end() {
+    echo '</main>';
 }
--- inc/setup/menus-sidebars.php
+++ inc/setup/menus-sidebars.php
@@ -38,3 +38,16 @@ function edgeframe_register_sidebars() {
 		)
 	);
 }
+
+/**
+ * Register theme widgets.
+ */
+function edgeframe_register_widgets() {
+    if ( class_exists( 'EdgeFrame_Widget_About' ) ) {
+        register_widget( 'EdgeFrame_Widget_About' );
+    }
+    if ( class_exists( 'EdgeFrame_Widget_Recent_Posts' ) ) {
+        register_widget( 'EdgeFrame_Widget_Recent_Posts' );
+    }
+}
+add_action( 'widgets_init', 'edgeframe_register_widgets' );
